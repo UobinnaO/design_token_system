1ffc1427eb4a1e14b24bbd357e3e6169d20a4a00
uobinnao <uobinnao@gmail.com>
2025-07-12 16:52:35 -0400

    init setup before npm/dependencies install

---
+++ design_tokens/config.js
@@ -0,0 +1,42 @@
+import StyleDictionary from "style-dictionary";
+
+const isHigherTierToken = (filePath) => {
+  return (
+    filePath.includes("tier-2-usage") || filePath.includes("tier-3-component")
+  );
+};
+
+StyleDictionary.registerFormat({
+  name: "css/custom-variables",
+  format: function ({ dictionary, platform }) {
+    return `:root {
+${dictionary.allTokens
+  .map((token) => {
+    const tokenName = token.name.replace(`${platform.prefix}-`, "");
+    if (isHigherTierToken(token.filePath)) {
+      return `  --${platform.prefix}-theme-${tokenName}: ${token.value};`;
+    }
+    return `  --${platform.prefix}-${tokenName}: ${token.value};`;
+  })
+
+  .join("\n")}
+}`;
+  },
+});
+
+export default {
+  source: ["design_tokens/**/*.json"],
+  platforms: {
+    css: {
+      prefix: "ds",
+      transformGroup: "css",
+      buildPath: "design_tokens/build/css/",
+      files: [
+        {
+          destination: "_variables.css",
+          format: "css/custom-variables",
+        },
+      ],
+    },
+  },
+};
